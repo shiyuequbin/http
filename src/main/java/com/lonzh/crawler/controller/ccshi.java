3c1d9136d32f6fe26f04468949748b23a7a2df0b
queben <m13709243799_1@163.com>
2019-06-21 21:30:54 +0800

    original+111

---
+++ src/main/java/com/lonzh/crawler/controller/ccshi.java
@@ -0,0 +1,19 @@
+package com.lonzh.crawler.controller;
+
+/**
+ * @ProjectName: Crawler
+ * @Package: com.lonzh.crawler.controller
+ * @ClassName: ccshi
+ * @Description: java类作用描述
+ * @Author: qubin
+ * @CreateDate: 2019/6/21 21:29
+ * @UpdateUser: 更新者
+ * @UpdateDate: 2019/6/21 21:29
+ * @UpdateRemark: 更新说明
+ * @Version: 1.0
+ */
+public class ccshi {
+    public static void main(String[] args) {
+        System.out.println("这是测试11111");
+    }
+}
