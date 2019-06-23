994fd126cd289e9784d1c566f94f5e886fe82f23
queben <m13709243799_1@163.com>
2019-06-23 13:55:24 +0800

    cecshi222222222

--- src/main/java/com/lonzh/crawler/controller/Ceshi3222222.java
+++ src/main/java/com/lonzh/crawler/controller/Ceshi3222222.java
@@ -3,18 +3,17 @@ package com.lonzh.crawler.controller;
 /**
  * @ProjectName: Crawler
  * @Package: com.lonzh.crawler.controller
- * @ClassName: ccshi
+ * @ClassName: Ceshi3222222
  * @Description: java类作用描述
  * @Author: qubin
- * @CreateDate: 2019/6/21 21:29
+ * @CreateDate: 2019/6/23 13:54
  * @UpdateUser: 更新者
- * @UpdateDate: 2019/6/21 21:29
+ * @UpdateDate: 2019/6/23 13:54
  * @UpdateRemark: 更新说明
  * @Version: 1.0
  */
-public class ccshi {
+public class Ceshi3222222 {
     public static void main(String[] args) {
-        System.out.println("这是测试11111");
-        System.out.println("这是第二次测试");
+        System.out.println("这是测试222222222");
     }
 }
