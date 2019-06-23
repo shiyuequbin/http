d71a61504b62a5dd98aea4cb930065a076dc0a2b
queben <m13709243799_1@163.com>
2019-06-23 22:30:53 +0800

    2222222222

--- src/main/java/com/lonzh/crawler/controller/Ceshi1111.java
+++ src/main/java/com/lonzh/crawler/controller/Ceshi1111.java
@@ -1,5 +1,11 @@
 package com.lonzh.crawler.controller;
 
+import com.sun.xml.internal.ws.api.pipe.NextAction;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+
 /**
  * @ProjectName: Crawler
  * @Package: com.lonzh.crawler.controller
@@ -15,5 +21,15 @@ package com.lonzh.crawler.controller;
 public class Ceshi1111 {
     public static void main(String[] args) {
         System.out.println("这是第一次修改");
+        ArrayList<String> list = new ArrayList<>();
+        HashMap<String, String> map = new HashMap<>();
+        Iterator<String> it = list.iterator();
+        while (it.hasNext()){
+            String next = it.next();
+            if(next.equals("1")){
+                it.remove();
+                continue;
+            }
+        }
     }
 }
