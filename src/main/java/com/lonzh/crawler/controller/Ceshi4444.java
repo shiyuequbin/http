10bdc877ea2d545c05a635eb08e73aff521b6310
queben <m13709243799_1@163.com>
2019-06-23 22:41:15 +0800

    44444

---
+++ src/main/java/com/lonzh/crawler/controller/Ceshi4444.java
@@ -0,0 +1,16 @@
+package com.lonzh.crawler.controller;
+
+/**
+ * @ProjectName: Crawler
+ * @Package: com.lonzh.crawler.controller
+ * @ClassName: Ceshi4444
+ * @Description: java类作用描述
+ * @Author: qubin
+ * @CreateDate: 2019/6/23 22:40
+ * @UpdateUser: 更新者
+ * @UpdateDate: 2019/6/23 22:40
+ * @UpdateRemark: 更新说明
+ * @Version: 1.0
+ */
+public class Ceshi4444 {
+}
